d6713c7610c2bc251c34c43bf190bfba8c5371af
AlmondDacquoise <47581747+AlmondDacquoise@users.noreply.github.com>
2020-02-01 12:21:57 -0500

    links

--- src/App.js
+++ src/App.js
@@ -1,35 +1,66 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import businessform from './businessform.js'
+
+class Example extends Component {
+  render () {
+    return (
+      <div>
+      <h1>Example page</h1>
+      <Link to="/businessform">Go back home</Link>
+      </div>
+      
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <main>
+      <BrowserRouter>
       <Switch>
-      <Route path="/" component={businessform} exact />
+      <Route path="/businessform" component={businessform} exact />
+      <Route path="/example" component={Example} exact />
       </Switch>
-      </main>
-      <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      </BrowserRouter>
+      )
+  }  
+}
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <main>
+//       <h1>
+//         hi
+//       </h1>
+//       // <BrowserRouter>
+//       //   <Switch>
+//       //     <Route path="/" component={businessform} exact />
+//       //   </Switch>
+//       // </BrowserRouter>
+//       </main>
+//       // <div className="App">
+//       // <header className="App-header">
+//       // <img src={logo} className="App-logo" alt="logo" />
 
 
-      <p>
-      Welcome to Summation
-      <p>
-      I am a...
-      </p>
-      </p>
-      <buttonGroup variant="text" color="primary" aria-label="text primary button group">
-      <button>Business</button>
-      <button>Charity</button>
-      </buttonGroup>
-      </header>
-      </div>
-      );
-    }
-  }
+//       // <p>
+//       // Welcome to Summation
+//       // <p>
+//       // I am a...
+//       // </p>
+//       // </p>
+//       // <buttonGroup variant="text" color="primary" aria-label="text primary button group">
+//       // <button>Business</button>
+//       // <button>Charity</button>
+//       // </buttonGroup>
+//       // </header>
+//       // </div>
+//       );
+//     }
+//   }
 
-  export default App;
+export default App;
