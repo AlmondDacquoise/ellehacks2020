c82ac71bb8f6c41c15906a6b58e101096c22d466
AlmondDacquoise <47581747+AlmondDacquoise@users.noreply.github.com>
2020-02-01 11:49:50 -0500

    mmlml

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-  return (
-    <div className="App">
-    <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
+    return (
+      <main>
+      <Switch>
+      <Route path="/" component={businessform} exact />
+      </Switch>
+      </main>
+      <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
 
 
-    <p>
-    Welcome to Summation
-    <p>
-    I am a...
-    </p>
-    </p>
-    <buttonGroup variant="text" color="primary" aria-label="text primary button group">
-    <button>Business</button>
-    <button>Charity</button>
-    </buttonGroup>
-    </header>
-    </div>
-    );
+      <p>
+      Welcome to Summation
+      <p>
+      I am a...
+      </p>
+      </p>
+      <buttonGroup variant="text" color="primary" aria-label="text primary button group">
+      <button>Business</button>
+      <button>Charity</button>
+      </buttonGroup>
+      </header>
+      </div>
+      );
+    }
   }
-}
 
   export default App;
